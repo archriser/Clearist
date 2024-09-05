847d53277765bac2a980ceb056686f50c2702b26
Tasdik <161122121+tasdikbyousuf@users.noreply.github.com>
2024-09-06 00:23:39 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Clyde
+ A simple command-line to-do list manager for organizing and tracking tasks efficiently.
---
+++ main.py
@@ -0,0 +1,12 @@
+def addTask():
+    pass
+def removeTask():
+    pass
+def editTask():
+    pass
+def listAll():
+    pass
+def listIncomplete():
+    pass
+def listComplete():
+    pass
